171c7ba3fe5d015114e6b52d43e3f492f0c7ad34
mario <marioiram2000@gmail.com>
2021-11-09 11:52:18 +0100

    commit

---
+++ proyecto2/src/module-info.java
@@ -0,0 +1,12 @@
+module proyecto2 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires java.desktop;
+	requires java.sql;
+
+	opens application to javafx.graphics, javafx.fxml;
+	opens controller to javafx.graphics, javafx.fxml;
+	opens modelo to javafx.base;
+}
